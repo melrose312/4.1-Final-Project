192d7049b5fc69c31dc99ddc2c73736a400635c3
melrose312 <melrose312@gmail.com>
2025-11-25 13:18:23 -0800

    2nd Page JS Logic Troubleshoot

--- movie.js
+++ movie.js
@@ -1,10 +1,18 @@
+const movieContainer = document.querySelector('.movies');
 
-async function main() {
-    const movieInfo = localStorage.getItem('movieInfo')
-    const poster = await fetch (`http://img.omdbapi.com/?apikey=4c277607&localStorage.getItem('movieInfo')`)
+async function displayMovie() {
+    const movieData = localStorage.getItem('movieInfo');
+    const poster = await fetch (`http://www.omdbapi.com/?apikey=4c277607&i=${movieData.imdbID}`)
     const posterImg = await poster.json();
-
     console.log(posterImg)
+
+    movieContainer.innerHTML= posterImg.map(poster => `
+      <img class="movie__img" src="${moviesData.Poster}" alt="${moviesData.Title}">
+      <div class="movie__title">${moviesData.Title}</div>
+      <div class="movie__year">${moviesData.Year}</div>
+      `).join('');
+
 }
 
-main();
+displayMovie(); 
+
